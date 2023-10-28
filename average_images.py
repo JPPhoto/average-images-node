59a35354ec9abb22e3fd673a6f59515c7f5129f1
JPPhoto <jpollack@jpollackphoto.com>
2023-10-28 08:42:55 -0500

    Added gamma correction

--- average_images.py
+++ average_images.py
@@ -20,6 +20,7 @@ class AverageImagesInvocation(BaseInvocation, WithMetadata, WithWorkflow):
     """Average images"""
 
     images: list[ImageField] = InputField(description="The collection of images to average")
+    gamma: float = InputField(default=2.2, gt=0.0, description="Gamma for color correcting before/after blending")
 
     def invoke(self, context: InvocationContext) -> ImageOutput:
         if len(self.images) == 0:
@@ -32,12 +33,15 @@ class AverageImagesInvocation(BaseInvocation, WithMetadata, WithWorkflow):
             image = image.convert("RGB")
             if arr is None:
                 w, h = image.size
-                arr = np.zeros((h, w, 3), np.uint64)
+                arr = np.zeros((h, w, 3), np.float32)
 
-            imarr = np.array(image, dtype=np.uint64)
+            imarr = np.array(image)
+            imarr.astype(np.float32)
+            imarr = (imarr / 255.0) ** (self.gamma)
             arr += imarr
 
         arr = arr / len(self.images)
+        arr = np.clip(arr ** (1.0 / self.gamma), 0, 1) * 255.0
         arr = arr.astype(np.uint8)
         image = Image.fromarray(arr, mode="RGB")
 
